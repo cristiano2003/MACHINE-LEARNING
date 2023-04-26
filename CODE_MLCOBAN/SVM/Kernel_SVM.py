f0f1e166f14a8d589f35bf95c56f5a2e225226f1
cristiano2003 <nguyentrungtruc2003@gmail.com>
2023-04-26 22:37:10 +0700

    first

---
+++ CODE_MLCOBAN/SVM/Kernel_SVM.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+
+# XOR dataset and targets
+X = np.array([[0, 0], [1, 1], [1, 0], [0, 1]])
+Y = np.array([0 ,0, 1, 1])
+# fit the model
+for kernel in ('sigmoid', 'poly', 'rbf'):
+    clf =  svm.SVC(kernel = kernel, gamma = 4, coef0 = 0)
+    clf.fit(X, Y)
